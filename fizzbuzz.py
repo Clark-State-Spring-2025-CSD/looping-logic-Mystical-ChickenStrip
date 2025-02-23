4081d413200240d3cdee9a8c0a30ff750208b2be
SEAN ALLEN <allens4604@clarkstate.edu>
2025-02-23 14:38:54 -0500

    Fizz Buzz (23 FEB 25)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,3 +13,14 @@
 #13
 #14
 #FizzBuzz
+
+for num in range(1, 26):  # Loop through integers 1 to 25
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
